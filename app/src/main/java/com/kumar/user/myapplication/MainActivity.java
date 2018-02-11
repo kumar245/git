1cdf31c6e37bde6256e353aae5bd90a0e071162f
kumar <popurichaitanyakumar@gmail.com>
2018-02-11 16:35:53 -0500

    ima conflict

--- app/src/main/java/com/kumar/user/myapplication/MainActivity.java
+++ app/src/main/java/com/kumar/user/myapplication/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int a;
+    int c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a=5;
+        c=6;
+
     }
 }
