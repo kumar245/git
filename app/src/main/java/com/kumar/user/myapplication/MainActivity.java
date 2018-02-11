ef645d94d769ee9925f0e66766e276cc22602f48
kumar <popurichaitanyakumar@gmail.com>
2018-02-11 16:34:35 -0500

    second conflict

--- app/src/main/java/com/kumar/user/myapplication/MainActivity.java
+++ app/src/main/java/com/kumar/user/myapplication/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    int a;
+    int b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        a=0;
+        b=1;
     }
 }
